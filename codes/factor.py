634dcbf347404262a316cd4c93a5affe39228454
algo21-221040010 <221040010@link.cuhk.edu.cn>
2022-05-06 01:11:13 +0800

    add factor calculate functions

--- codes/factor.py
+++ codes/factor.py
@@ -11,55 +11,81 @@ from data_handle import *
 from signal_handle import *
 
 
-# 计算因子 北向资金的增量
-def get_factor(data, north_buy_sell, future_data):
-    """ 求北向因子： factor = 对所有成分股 Σ[( OIt - OI(t-1) ) *Pt]
-        其中OI为沪港通、深港通的单个股票交易量数据, P为股票收盘价
+def calc_AMA(data, n, calc='fq_close',fastlen=2, slowlen=30):
+    '''
+    Function
+        计算AMA（卡夫曼自适应移动平均）
+            1. 价格方向direction = price –price[n]; 其中price[n]是n个周期前的收盘价。
+            2. 波动性 volatility = @sum(@abs(price –price[1]), n);@sum(value, n)是n个周期中的数值之和函数。
+            即，此处波动定义为：所有“日到日”的价格绝对变化的总和，在同样的n个周期上。
+            3. 效率系数（ER）Efficiency_Ratio = direction/volativity;
+                方向移动对噪音之比，称之为效率系数，该系数的值就从0到1 变化。
+            4. 每天的均线速度可以用改变平滑系数来改变，成为自适应性的。该公式如下：
+                EXPMA = EXPMA[1] + c*(price –EXPMA[1]);
+            这个平滑系数是：
+                fastest = 2/(N+1) = 2/(2+1) =0.6667;
+                slowest = 2/(N+1) = 2/(30+1) =0.0645;
+                smooth = ER*(fastest - slowest)+ slowest;
+                c = smooth*smooth;
+            平方平滑系数迫使c的数值趋向于零。即，较慢的移动平均线的权重大于快的移动平均值。就像出现不确定状况时就更加保守一样。
+                AMA = AMA[1] + c*(price –AMA[1]);
+    Parameters
+    Return
+    '''
+    data['direction'] = data[calc] - data[calc].shift(n)
+    data['volatility'] = abs(data[calc].diff()).rolling(n).sum()
+    data['ER'] = np.divide(data['direction'], data['volatility'])
+    fast = 2/(fastlen + 1)
+    slow = 2/(slowlen + 1)
+    data['c'] = np.square((fast - slow)*data['ER']+slow)
+    data.dropna(axis=0,inplace=True)
+    # 计算 AMA： AMA = AMA[1] + c*(price –AMA[1])
+    ama_list = []
+    ama_list.append(data[calc].iloc[0])
+    for i in range(data.shape[0]):
+        ama = ama_list[-1] + data['c'].iloc[i] *(data[calc].iloc[i]- ama_list[-1])
+        ama_list.append(ama)
+    del ama_list[0]
+    data['AMA'+str(n)] = ama_list
+    return data
 
-    Args:
-        data (dataframe): 股指成分股中 开通了沪港通、深港通的所有成分股的 行情数据
-        north_buy_sell (dataframe): 北向的买入、卖出金额数据
-        future_data (dataframe): 复权后的日度期货数据
 
-    Returns:
-        dataframe: 因子数据['date','factor']
-    """
-    ### 计算因子值：factor = 对所有成分股 Σ[(OIt - OI(t-1))*Pt] / Σ[amount(t)]
-    data = data.set_index(['date','code','close','amount'])
-    data = data.groupby(['code'])['oi'].diff()
-    data.name = 'delta_oi'
-    data = data.reset_index()
-    # 第一天的dalta，以及新纳入股票的第一天的delta会是空值，此处以 0 填充
-    data.fillna(0, inplace=True)
+def calc_BMA(data, L, calc='fq_close'):
+    data['BMA']= data[calc].rolling(L).mean()
+    return data
+
+
+def calc_pvResonance_V1(data,calc_p='fq_close',calc_v='all_volume', shortLen=5, longLen=100, L=50, N=3):
+    # 对 量
+    data = calc_AMA(data, shortLen, calc=calc_v)
+    data = calc_AMA(data, longLen, calc =calc_v)
+    data['v'] = data['AMA'+str(shortLen)] / data['AMA'+str(longLen)]
+    # 对 价
+    data = calc_BMA(data, L, calc=calc_p)
+    data['p'] = data['BMA'] / data['BMA']
+
+    data['factor_pv'] = data['p'] * data['v']
+    return data
+
+
+def get_trading_sig_V1(data_factor,factor='factor_pv',s=1.10):
+    '''
+    Function
+    Parameter
+        data    
+    Return
+        data    [dateframe]        信号数据（字段 +['sig']）
+    '''
+    # 价量共振指标大于 s ，买进。否则，卖出。
+    data_factor['pre_'+factor] = data_factor[factor].shift(1).fillna(0)
     
-    # 按 date groupby
-    grouped = data.groupby(['date'])
-    factor_data = grouped.apply(lambda x: np.divide(np.multiply(x['delta_oi'],x['close']).sum(), x['amount'].sum()) )
-    factor_data.name = 'factor'
-    factor_data = factor_data.reset_index()
-
-    future_data = future_data.drop('factor', axis=1)
-    data_factor = pd.merge(factor_data, future_data, on='date', how='left')
-    data_factor['date_time'] = data_factor.apply(lambda x:datetime.datetime.strptime(str(x['date'])\
-                    ,'%Y%m%d'), axis=1) 
-    # 加上北向的买入、卖出金额数据
-    data_factor = pd.merge(data_factor, north_buy_sell[['date_time','buy','sell']], on='date_time', how='left')
-    data_factor['inflow_tense'] = data_factor['factor']/(data_factor['buy']+data_factor['sell'])
-    print('存在数据缺失的日期：', list(data_factor[(data_factor['inflow_tense'].isnull())]['date']))
-    return data_factor
-
-
-def get_trading_sig(data_factor,s1=60,s_1=-40): 
-    """计算买卖信号
-
-    Args:
-        data_factor (dateframe): 因子数据（字段['factor']）
-        s1 (int, optional): 因子阈值. Defaults to 60.
-        s_1 (int, optional): 因子阈值. Defaults to -40.
-
-    Returns:
-        _type_: _description_
-    """
+    # macd > 0, 买入; macd < 0; 卖出
+    data_factor['sig'] = data_factor.apply(lambda x:1 if (x[factor]>s) #and x['pre_factor']<s
+        #and x['AMA'+str(shortLen)]>0 and x['AMA'+str(longLen)]>0)
+        else(-1 if (x[factor]<=s)  #and x['pre_factor']>s
+        #or (x['AMA'+str(shortLen)]<0 or x['AMA'+str(longLen)]<0)) 
+        else 0), axis=1)
+    data_factor.drop(['pre_'+factor], axis=1, inplace=True)
     data_factor = adjust_trading_sig(data_factor)
     return data_factor
 
